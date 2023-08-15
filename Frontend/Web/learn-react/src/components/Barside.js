85fb48d6a0ace0761cc5a3dabc26855510fa3c7c
Youssef Assoil <assoilyoussef02@gmail.com>
2023-08-15 16:32:40 +0100

    WEBAPP YOUSSEF'S UPDATE

--- Frontend/Web/learn-react/src/components/Barside.js
+++ Frontend/Web/learn-react/src/components/Barside.js
@@ -1,35 +1,30 @@
 import React from "react";
+import { Link } from "react-router-dom"; // Import Link
 import '../css/styles.css';
 
 const nav = [
   { text: "Home", link: "#!", icon: "home" },
   { text: "About", link: "#!", icon: "box" },
-  { text: "Movies", link: "#!", icon: "tv", active: true },
-  { text: "Contacts", link: "#!", icon: "user" },
-  { text: "Bobliothèque", link: "#!", icon: "book" },
-  { text: "Cartographie", link: "#!", icon: "map-signs" },
-  { text: "Nature", link: "#!", icon: "leaf" }
+  { text: "Demande Medecin", link: "/DemandeMedcin", icon: "book" },
 ];
 
 const Sidebar = ({ toggleBtn }) => {
   return (
-    <div
-      className={`${toggleBtn ? "sidebar collapse" : "sidebar"}`}
-      data-simplebar
-    >
+    <div className={`${toggleBtn ? "sidebar collapse" : "sidebar"}`} data-simplebar>
       <ul>
         {nav.map(item => (
           <li key={item.text}>
-            <a href="#!" className={item.active ? "active" : ""}>
+            <Link to={item.link} className={item.active ? "active" : ""}>
               <span className="icon">
                 <i className={`fas fa-${item.icon}`} />
               </span>
               <span className="title">{item.text}</span>
-            </a>
+            </Link>
           </li>
         ))}
       </ul>
     </div>
   );
 };
+
 export default Sidebar;

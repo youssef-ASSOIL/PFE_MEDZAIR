f7c98a934deaf8cf200428db24faba912e8ba52f
Youssef Assoil <assoilyoussef02@gmail.com>
2023-09-02 16:25:45 +0100

    update List Hospitals

--- Frontend/Web/learn-react/src/components/Administrateur/ListerHospitals.js
+++ Frontend/Web/learn-react/src/components/Administrateur/ListerHospitals.js
@@ -1,8 +1,60 @@
-import React from 'react'
+import React from 'react';
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Avatar } from '@mui/material';
+import "../../css/ListerHospital.css"
+const columns = [
+  { id: 'email', label: 'Email' },
+  { id: 'imagePath', label: 'Image Path' },
+  { id: 'name', label: 'Name' },
+  { id: 'region', label: 'Region' },
+];
+
+function createData(email, imagePath, name, region) {
+  return { email, imagePath, name, region };
+}
+
+
+ const doctors = [
+    {
+      email: "example1@email.com",
+      image: "https://media.licdn.com/dms/image/D4E03AQGRhUj6l8vfBg/profile-displayphoto-shrink_800_800/0/1665924231705?e=2147483647&v=beta&t=5h4furBSItPLL7Rlubqh_H8ZiLpIKnqBy_p3fNd3ehs",
+      hospital: "Hospital A",
+      region: "Region X",
+    },
+    {
+        email: "example1@email.com",
+        image: "https://media.licdn.com/dms/image/C5603AQFmm8_egAcZpA/profile-displayphoto-shrink_200_200/0/1641904982500?e=1697673600&v=beta&t=TTrnnIEURMiSBvg-JSsqRl3m4wdhZx0hL8KtC7PTn3A",
+        hospital: "KETTANI Clinique",
+        region: "Region X",
+      },
+    // Add more doctor objects as needed
+  ];
 
 export default function ListerHospitals() {
   return (
-    <div>ListerHospitals</div>
-  )
+    <TableContainer component={Paper} className="TableContainer">
+      <Table>
+        <TableHead className="TableHead">
+          <TableRow>
+            
+            <TableCell className="TableCell">Email</TableCell>
+            <TableCell className="TableCell">Image</TableCell>
+            <TableCell className="TableCell">Hospital</TableCell>
+            <TableCell className="TableCell">Region</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody className="TableBody">
+          {doctors.map((doctor, index) => (
+            <TableRow key={index}>
+              <TableCell className="TableCell">{doctor.hospital}</TableCell>
+              <TableCell className="TableCell">{doctor.email}</TableCell>
+              <TableCell className="TableCell">
+                <Avatar alt={doctor.name} src={doctor.image} className="Avatar" />
+              </TableCell>
+              <TableCell className="TableCell">{doctor.region}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
 }
-
